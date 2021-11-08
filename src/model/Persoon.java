2292e9614630a2a7a31b528b43fd814b43ad5c7e
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-08 14:29:42 +0100

    Oefening 2.1.2 Update aan de hand van klasse diagram

--- src/model/Persoon.java
+++ src/model/Persoon.java
@@ -5,35 +5,75 @@ package model;
  * Beschrijft een persoon die werkzaamheden voor ons bedrijf verricht.
  */
 public class Persoon {
+    private static final double GRENSWAARDE_BONUS = 4500.0;
+    private static final String DEFAULT_NAAM = "Onbekend";
+    private static final String DEFAULT_WOONPLAATS = "Onbekend";
+    private static final int DEFAULT_MAAND_SALARIS = 0;
+
     public static int aantalPersonen = 0;
 
-    public int personeelsNummer;
-    public String naam;
-    public String woonplaats;
-    public double maandSalaris;
+    private int personeelsNummer;
+    private String naam;
+    private String woonplaats;
+    private double maandSalaris;
 
     public Persoon(String naam, String woonplaats, double maandSalaris) {
-        this.naam = naam;
+        this.setNaam(naam);
         this.woonplaats = woonplaats;
-        this.maandSalaris = maandSalaris;
+        this.setMaandSalaris(maandSalaris);
         this.personeelsNummer = ++aantalPersonen;
     }
 
     public Persoon(String naam) {
-        this.naam = naam;
-        this.woonplaats = "Onbekend";
-        this.maandSalaris = 0;
-        this.personeelsNummer = ++aantalPersonen;
+        this(naam, DEFAULT_WOONPLAATS, DEFAULT_MAAND_SALARIS);
     }
 
     public Persoon() {
-        this.naam = "Onbekend";
-        this.woonplaats = "Onbekend";
-        this.maandSalaris = 0;
-        this.personeelsNummer = ++aantalPersonen;
+        this(DEFAULT_NAAM);
     }
 
     public double berekenJaarInkomen() {
         return 12 * maandSalaris;
+    }
+
+    public boolean heeftRechtOpBonus(){
+        return maandSalaris >= GRENSWAARDE_BONUS;
+    }
+
+    public int getPersoneelsNummer() {
+        return personeelsNummer;
+    }
+
+    public void setPersoneelsNummer(int personeelsNummer) {
+        this.personeelsNummer = personeelsNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public double getMaandSalaris() {
+        return maandSalaris;
+    }
+
+    public void setMaandSalaris(double maandSalaris) {
+        if (maandSalaris < 0) {
+            System.out.println("Het maandsalaris mag niet negatief zijn, het wordt nu op 0 gezet.");
+            this.maandSalaris = 0;
+        } else {
+            this.maandSalaris = maandSalaris;
+        }
     }
 }
