076f9bdfa6126d4353f9d43b8563e0a8e993e04a
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-08 14:42:43 +0100

    Oefening 2.1.4 uitvoer met printf en ternary operator

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -24,7 +24,15 @@ public class BedrijfLauncher {
         Persoon manager = new Persoon();
 
         System.out.println(Persoon.aantalPersonen);
-        System.out.printf("%s verdient %.2f per jaar\n", baas.getNaam(), baas.berekenJaarInkomen());
-        System.out.printf("%s woont in %s\n", assistent.getNaam(), assistent.getWoonplaats());
+
+        // Zo kon het ook
+//        String rechtOpBonus = "geen";
+//        if (baas.heeftRechtOpBonus()) {
+//            rechtOpBonus = "wel";
+//        }
+        System.out.printf("%s verdient %.2f en heeft %s recht op een bonus.\n",
+                baas.getNaam(), baas.getMaandSalaris(), baas.heeftRechtOpBonus() ? "wel" : "geen"); // ternairy operator
+        System.out.printf("%s verdient %.2f en heeft %s recht op een bonus.\n",
+                medewerker.getNaam(), medewerker.getMaandSalaris(), medewerker.heeftRechtOpBonus() ? "wel" : "geen");
     }
 }
