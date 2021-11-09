c6749631673434b771e8975dc00f44a48bdc7785
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-09 09:47:22 +0100

    Oefening 3.1.3 Afdeling klasse toegevoegd

---
+++ src/model/Afdeling.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * Beschrijft een afdeling binnen ons bedrijf
+ */
+public class Afdeling {
+    private static final String DEFAULT_AFDELINGS_NAAM = "Onbekend";
+    private static final String DEFAULT_AFDELINGS_PLAATS = "Onbekend";
+
+    private String afdelingsNaam;
+    private String afdelingsPlaats;
+
+    public Afdeling(String afdelingsNaam, String afdelingsPlaats) {
+        this.afdelingsNaam = afdelingsNaam;
+        this.afdelingsPlaats = afdelingsPlaats;
+    }
+
+    public Afdeling() {
+        this(DEFAULT_AFDELINGS_NAAM, DEFAULT_AFDELINGS_PLAATS);
+    }
+}
